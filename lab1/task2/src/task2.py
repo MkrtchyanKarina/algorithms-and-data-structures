745c553b859cd654947b0b0266f3229ef5cbe6c9
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2024-09-25 21:31:55 +0300

    added new tests

--- lab1/task2/src/task2.py
+++ lab1/task2/src/task2.py
@@ -1,10 +1,3 @@
-import psutil
-import time
-import random
-
-t_start = time.perf_counter()
-
-
 def insertion_sort(n, m):
     new_m = []
     indexes = [1]
@@ -18,23 +11,15 @@ def insertion_sort(n, m):
         indexes.append(i+1)
     return indexes, m
 
-# print(sort_v(10, [1, 8, 4, 2, 3, 7, 5, 6, 9, 0]))
-test_max_m = [random.randint(-10**9, 10**9) for i in range(10**3)]
-max_result = insertion_sort(10 ** 3, test_max_m)
-print("Время работы: %s секунд" % (time.perf_counter() - t_start))
-print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
-print(" ".join(map(str, max_result)))
 
-
-
-file_input = open("input2.txt")
-file_output = open("output2.txt", "w")
-
-test_n = int(file_input.readline())
-test_m = list(map(int, file_input.readline().split(" ")))
-
-res_ind, res_m = insertion_sort(test_n, test_m)
-
-file_output.write(" ".join(map(str, res_ind)))
-file_output.write("\n")
-file_output.write(" ".join(map(str, res_m)))
+# file_input = open("input2.txt")
+# file_output = open("output2.txt", "w")
+#
+# test_n = int(file_input.readline())
+# test_m = list(map(int, file_input.readline().split(" ")))
+#
+# res_ind, res_m = insertion_sort(test_n, test_m)
+#
+# file_output.write(" ".join(map(str, res_ind)))
+# file_output.write("\n")
+# file_output.write(" ".join(map(str, res_m)))
