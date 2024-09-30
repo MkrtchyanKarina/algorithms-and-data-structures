398192ba9e8106cdb82cee410e34071f20bf30af
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2024-09-30 17:47:47 +0300

    add new tests

--- lab1/task6/src/task6.py
+++ lab1/task6/src/task6.py
@@ -1,22 +1,20 @@
-import psutil
-import time
-import random
 from lab1.src.verifications import data_verification1
-t_start = time.perf_counter()
+
 
 @data_verification1
 def bubble_sort(n, m):
     for i in range(n):
-        for j in range(i+1, n):
-            if m[i] > m[j]:
-                m[i], m[j] = m[j], m[i]
+        # print(m[i], m)
+        for j in range(0, n-i-1):
+
+            if m[j] > m[j+1]:
+                m[j], m[j+1] = m[j+1], m[j]
     return m
 
 
-test_max_m = [random.randint(-10**9, 10**9) for i in range(10**3)]
-max_result = bubble_sort(10 ** 3, test_max_m)
-if max_result == sorted(test_max_m):
-    print("Время работы: %s секунд" % (time.perf_counter() - t_start))
-    print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
-    print(" ".join(map(str, max_result)))
+file = open("input6.txt")
+test_n = int(file.readline())
+test_m = list(map(int, file.readline().split(" ")))
+open("output6.txt", "w").write(" ".join(map(str, bubble_sort(test_n, test_m))))
+
 
