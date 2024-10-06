f42c7085c9a0be0e5dd1f559c126154f938e7c65
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2024-10-06 18:54:40 +0300

    add new files

--- lab2/task8.py
+++ lab2/task8.py
@@ -1,80 +1,65 @@
-C = [0] * 9
-A = [2, -1, 4, 3, -5]
-B = [1, -7, 2, 1, -3]
+import time
+
+import psutil
+
+A = [2, -1, 4, 3, -5, 8, 2, 3, 19, 5]
+B = [1, -7, 2, 1, -3, -2, 2, -1, 4, 3]
+C = [0] * (len(A) * 2 - 1)
 def multiply(ind_a, ind_b):
-
+    # print(ind_a, ind_b)
     global A, B, C
-    if ind_b is None:
-        a, b = ind_a
-        c, d = ind_a
-        C[a + c] += A[a] * B[c]
-        C[b + d] += A[b] * B[d]
-        C[a + d] += A[a] * B[d] + A[b] * B[c]
+    if ind_a is None and ind_b is None:
+        return C
+    elif ind_b is None:
+        for i in ind_a:
+            for j in ind_a:
+                C[i+j] += A[i] * B[j]
+    elif ind_a is None:
+        for i in ind_b:
+            for j in ind_b:
+                C[i+j] += A[i] * B[j]
     else:
-        if len(ind_a) == 2:
-            a, b = ind_a
-            c, d = ind_a
-            C[a + c] += A[a] * B[c]
-            C[b + d] += A[b] * B[d]
-            C[a + d] += A[a] * B[d] + A[b] * B[c]
+        for i in ind_a:
+            for j in ind_a:
+                C[i+j] += A[i] * B[j]
+        for i in ind_b:
+            for j in ind_b:
+                C[i+j] += A[i] * B[j]
 
-            a, b = ind_a
-            c, d = ind_b
-            C[a + c] += A[a] * B[c]
-            C[b + d] += A[b] * B[d]
-            C[a + d] += A[a] * B[d] + A[b] * B[c]
-
-            a, b = ind_b
-            c, d = ind_a
-            C[a + c] += A[a] * B[c]
-            C[b + d] += A[b] * B[d]
-            C[a + d] += A[a] * B[d] + A[b] * B[c]
-
-        else:
-            a = b = ind_a[0]
-            C[a + b] += A[a] * B[b]
-
-            a = ind_a[0]
-            c, d = ind_b
-            C[a + c] += A[a] * B[c]
-            C[a + d] += A[a] * B[d]
-
-            a = ind_a[0]
-            c, d = ind_b
-            C[a + c] += A[c] * B[a]
-            C[a + d] += A[d] * B[a]
-
-
-        a, b = ind_b
-        c, d = ind_b
-        C[a + c] += A[a] * B[c]
-        C[b + d] += A[b] * B[d]
-        C[a + d] += A[a] * B[d] + A[b] * B[c]
+        for i in ind_a:
+            for j in ind_b:
+                C[i+j] += A[i] * B[j]
+                C[i + j] += A[j] * B[i]
+    # for i in ind_b:
+    #     for j in ind_a:
+    #         C[i+j] += A[i] * B[j]
 
 
 
-    print(C)
-
-
-def merge_sort(len_arr, array):
+def merge(len_arr, array):
 
     middle = len_arr // 2
     list_a, list_b = array[:middle], array[middle:]
 
     len_a, len_b = middle, len_arr - middle
+
     if len_a > 2:
-        list_a =  merge_sort(len_a, list_a)
+        list_a = merge(len_a, list_a)
     if len_b > 2:
-        list_b =  merge_sort(len_b, list_b)
-    print(list_a, list_b)
-    multiply(list_a, list_b)
+        list_b = merge(len_b, list_b)
+
+
+    return multiply(list_a, list_b)
+
+# [2, -15, 15, -25, -25, 52, -67, -21, 36, -130, -16, 61, -73, -32, 57, 9, 80, 77, 15]
 
 
 
 
 
 
+t_start = time.perf_counter()
+print(merge(10, [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+print("Время работы: %s секунд" % (time.perf_counter() - t_start))
+print(f"Память: {psutil.Process().memory_info().rss / 1024 ** 2:.2f} МБ")
 
-
-
-merge_sort(5, [4, 3, 2, 1, 0])
